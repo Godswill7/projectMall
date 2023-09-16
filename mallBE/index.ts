0bb1c86fb9f18f2c38ead5da11404739bc633a3a
Godswill Udida <udidagodswill7@gmail.com>
2023-09-16 04:12:55 +0100

    pushing from udida

--- mallBE/index.ts
+++ mallBE/index.ts
@@ -1,1 +1,24 @@
-console.log("Start");
+import express, { Application } from "express";
+import { mainApp } from "./mainApp";
+
+const port: number = 6000;
+const app: Application = express();
+
+mainApp(app);
+
+const server = app.listen(port, () => {
+  console.log();
+  console.log("Server connection established");
+});
+
+process.on("uncaughtException", (error: Error) => {
+  console.log("uncaughtException: ", error);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason: any) => {
+  console.log("unhandledRejection: ", reason);
+  server.close(() => {
+    process.exit(1);
+  });
+});
---
+++ mallBE/mainApp.ts
@@ -0,0 +1,43 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import { HTTP, mainError } from "./error/mainError";
+import { errorHandler } from "./error/errorHandler";
+
+export const mainApp = (app: Application) => {
+  app.use(express.json());
+  app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "PATCH", "DELETE"],
+    })
+  );
+  app.use(morgan("dev"));
+  app.use(helmet());
+  app.set("view engine", "ejs");
+
+  app.get("/", (req: Request, res: Response) => {
+    try {
+      return res.status(200).json({
+        message: "OK response from server",
+      });
+    } catch (error: any) {
+      return res.status(404).json({
+        message: "error occured while loading default response",
+        data: error,
+      });
+    }
+  });
+  app.all("*", (req: Request, res: Response, next: NextFunction) => {
+    next(
+      new mainError({
+        name: "Router Error",
+        message: `This error is coming up because the  URL, isn't correct`,
+        status: HTTP.BAD,
+        success: false,
+      })
+    );
+  });
+  app.use(errorHandler);
+};
